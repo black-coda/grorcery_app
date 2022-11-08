9f2993d927ba1b922b8624654425a38f39be149f
Monday Solomon <83866674+black-coda@users.noreply.github.com>
2022-11-08 15:29:56 +0100

    app icon and name configured 🤞

--- lib/views/UI/themes/splash.dart
+++ lib/views/UI/themes/splash.dart
@@ -37,77 +37,81 @@ class ScreenOne extends StatefulWidget {
 class _ScreenOneState extends State<ScreenOne> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding:
-                EdgeInsets.only(right: MediaQuery.of(context).size.width / 30),
-            child: Row(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  right: MediaQuery.of(context).size.width / 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Image.asset("assets/leaf.png")],
+              ),
+            ),
+            const SizedBox(
+              height: 11,
+            ),
+            Image.asset('assets/logo.png'),
+            const SizedBox(
+              height: 32,
+            ),
+            SizedBox(
+              width: 316,
+              child: Text(
+                "Get your groceries delivered to your home",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            SizedBox(
+              width: 274,
+              child: Text(
+                'The best delivery app in town for delivering your daily fresh groceries',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              minWidth: 190.0,
+              height: 53,
+              padding: const EdgeInsets.all(16),
+              color: Theme.of(context).colorScheme.primary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Text(
+                "Shop Now",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ),
+            const SizedBox(
+              height: 0.5,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.end,
-              children: [Image.asset("assets/leaf.png")],
+              children: [Image.asset("assets/blurleaf.png")],
             ),
-          ),
-          const SizedBox(
-            height: 11,
-          ),
-          Image.asset('assets/logo.png'),
-          const SizedBox(
-            height: 32,
-          ),
-          SizedBox(
-            width: 316,
-            child: Text(
-              "Get your groceries delivered to your home",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          SizedBox(
-            width: 274,
-            child: Text(
-              'The best delivery app in town for delivering your daily fresh groceries',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline3,
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          MaterialButton(
-            onPressed: () {},
-            minWidth: 190.0,
-            height: 53,
-            padding: const EdgeInsets.all(16),
-            color: Theme.of(context).colorScheme.primary,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Text(
-              "Shop Now",
-              style: Theme.of(context).textTheme.headline2,
-            ),
-          ),
-          const SizedBox(
-            height: 0.5,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [Image.asset("assets/blurleaf.png")],
-          ),
-          Expanded(
-            child: Image.asset(
-              "assets/bottom_food.jpg",
-              color: Theme.of(context).appBarTheme.backgroundColor,
-              colorBlendMode: BlendMode.screen,
-            ),
-          )
-        ],
+            Expanded(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  "assets/bottom_food.png",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
