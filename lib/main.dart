dfada0e3d906093ac41691590cc7cd025bf96c2a
Monday Solomon <83866674+black-coda@users.noreply.github.com>
2024-02-14 08:27:48 +0100

    updated item_details.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: darkThemed,
       initialRoute: RouteManager.loginPage,
       onGenerateRoute: RouteManager.generateRoute,
+
     );
   }
 }
--- lib/views/UI/pages/item_details.dart
+++ lib/views/UI/pages/item_details.dart
@@ -3,180 +3,212 @@ import 'package:grorcery_app_/models/models.dart';
 
 // import 'items.dart';
 
-class ItemDetails extends StatelessWidget {
+class ItemDetails extends StatefulWidget {
   const ItemDetails({super.key});
+
+  @override
+  State<ItemDetails> createState() => _ItemDetailsState();
+}
+
+class _ItemDetailsState extends State<ItemDetails> {
+  int total = 1;
+  void increment() {
+    setState(() {
+      total = total + 1;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (total >= 2) {
+        total = total - 1;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const CustomCircle(),
-          const SizedBox(
-            height: 42,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: SizedBox(
-              height: 150,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Arabic Ginger",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            radius: 18,
-                            backgroundColor: const Color(0xff172C38),
-                            child: Icon(
-                              Icons.add,
-                              size: 18,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Text(
-                              "4",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          CircleAvatar(
-                            radius: 18,
-                            backgroundColor: const Color(0xff172C38),
-                            child: Icon(
-                              Icons.remove_sharp,
-                              size: 18,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  //Price Tag
-                  const Text(
-                    "1kg,  \$4",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 16,
-                      color: Color(
-                        0xffFF324B,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  //Description
-                  const Text(
-                    "Ginger is a flowering plant whose rhizome, ginger root or ginger, is widely used as a spice and a folk medicine.",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomCircle(),
+            const SizedBox(
+              height: 42,
             ),
-          ),
-          // const SizedBox(
-          //   height: 20.0,
-          // ),
-          SizedBox(
-            height: 180,
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
+            Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 2,
-                mainAxisExtent: 67.0,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16.0,
-              ),
-              itemCount: property.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: const Color(0xff172C38),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
+              child: SizedBox(
+                height: 150,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        SizedBox(
-                          height: 35,
-                          width: 35,
-                          child: Image.asset(property[index].imageUrl),
+                        const Text(
+                          "Arabic Ginger",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold),
                         ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              property[index].value,
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.primary,
-                                fontWeight: FontWeight.bold,
+                            CircleAvatar(
+                              radius: 18,
+                              backgroundColor: const Color(0xff172C38),
+                              child: IconButton(
+                                onPressed: increment,
+                                icon: Icon(
+                                  Icons.add,
+                                  size: 18,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
                               ),
                             ),
-                            Text(
-                              property[index].category,
-                              style: const TextStyle(
-                                color: Color(0xff617986),
-                                fontSize: 12,
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Text(
+                                "$total",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 18,
+                              backgroundColor: const Color(0xff172C38),
+                              child: GestureDetector(
+                                onTap: decrement,
+                                child: Icon(
+                                  Icons.remove_sharp,
+                                  size: 18,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
                               ),
                             ),
                           ],
-                        )
+                        ),
                       ],
                     ),
-                  ),
-                );
-              },
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    //Price Tag
+                    const Text(
+                      "1kg,  \$4",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
+                        color: Color(
+                          0xffFF324B,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    //Description
+                    const Text(
+                      "Ginger is a flowering plant whose rhizome, ginger root or ginger, is widely used as a spice and a folk medicine.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+            // const SizedBox(
+            //   height: 20.0,
+            // ),
+            SizedBox(
+              height: 180,
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2,
+                  mainAxisExtent: 67.0,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16.0,
+                ),
+                itemCount: property.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: const Color(0xff172C38),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 35,
+                            width: 35,
+                            child: Image.asset(property[index].imageUrl),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                property[index].value,
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                property[index].category,
+                                style: const TextStyle(
+                                  color: Color(0xff617986),
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
 
-          SizedBox(
-            width: 342,
-            height: 53,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.zero,
-                maximumSize: const Size(342, 53),
-                backgroundColor: Theme.of(context).colorScheme.primary,
-              ),
-              child: const Text(
-                "Add to Cart",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: SizedBox(
+                width: 342,
+                height: 53,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    maximumSize: const Size(342, 53),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: const Text(
+                    "Add to Cart",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
